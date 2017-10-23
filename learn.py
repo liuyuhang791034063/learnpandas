c4fa43924ff48aae29ac04a5dc40d0812e28af9f
liuyuhang791034063 <liuyuhang791034063@qq.com>
2017-10-23 21:53:05 +0800

    学习了可以输入给DataFrame构造器的数据

--- learn.py
+++ learn.py
@@ -1,9 +1,11 @@
 #coding=utf-8
-from pandas import DateFrame
+from pandas import DataFrame
 import pandas as pd
+import numpy as np
 
 data =  {'state':['ohio','Ohio','Ohio','Nevada','Nevada'],
          'year':[2000,2001,2002,2001,2002],
          'pop':[1.5,1.7,3.6,2.4,2.9]}
-frame = DateFrame(data)
+frame = DataFrame(data , columns=['state','year','pop','debt'])
+frame['debt'] = np.arange(5.)
 print frame
