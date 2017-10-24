d7c1f45c77835bb628e95d5f7335694c690b6eef
liuyuhang791034063 <liuyuhang791034063@qq.com>
2017-10-24 22:29:56 +0800

    学习了DataFrame与Series之间的索引和运算，但是不懂广播是什么，之后在看“

--- learn.py
+++ learn.py
@@ -1,13 +1,17 @@
 #coding=utf-8
-from pandas import DataFrame
+from pandas import DataFrame,Series
 import pandas as pd
 import numpy as np
 
 data =  {'state':['ohio','Ohio','Ohio','Nevada','Nevada'],
          'year':[2000,2001,2002,2001,2002],
          'pop':[1.5,1.7,3.6,2.4,2.9]}
-frame = DataFrame(data , columns=['state','year','pop','debt'])
+frame = DataFrame(data ,columns=['state','year','pop','debt'] ,index=['a','b','c','f','e'])
 frame['debt'] = np.arange(5.)
-print frame
-del frame['debt']
-print frame
+
+abc = DataFrame(np.arange(12.).reshape(4,3), columns=list('bfe'),index=['Utah','Ohio','Texas','Oregon'])
+
+de = abc.ix[2]
+series2 = Series(range(3), index=['b','e','f'])
+print series2
+print series2 + abc
