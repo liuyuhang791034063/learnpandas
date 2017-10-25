7e9b8983dbd748bf908a80b58a9a7fbaae62c513
liuyuhang791034063 <liuyuhang791034063@qq.com>
2017-10-25 21:17:35 +0800

    每日更新系列--还是DataFrame的一些基本操操作

--- learn.py
+++ learn.py
@@ -6,12 +6,12 @@ import numpy as np
 data =  {'state':['ohio','Ohio','Ohio','Nevada','Nevada'],
          'year':[2000,2001,2002,2001,2002],
          'pop':[1.5,1.7,3.6,2.4,2.9]}
-frame = DataFrame(data ,columns=['state','year','pop','debt'] ,index=['a','b','c','f','e'])
-frame['debt'] = np.arange(5.)
+frame = DataFrame({'b':[4,-3,np.nan,8], 'a':[10,-90,20,30]})
 
-abc = DataFrame(np.arange(12.).reshape(4,3), columns=list('bfe'),index=['Utah','Ohio','Texas','Oregon'])
+abc = DataFrame(np.arange(12).reshape(4,3), columns=list('bfe'),index=['One','Two','There','Four'])
 
 de = abc.ix[2]
-series2 = Series(range(3), index=['b','e','f'])
-print series2
-print series2 + abc
+series = Series([5,32,23,56], index=['b','a','e','f'])
+
+print frame
+print frame.describe()ttt
