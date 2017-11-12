f766c20704c86dd201926b0a1c712dd6737318af
liuyuhang791034063 <liuyuhang791034063@qq.com>
2017-11-12 23:03:42 +0800

    test7

--- test7.py
+++ test7.py
@@ -4,7 +4,5 @@ import pandas as pd
 from pandas import DataFrame, Series
 import numpy as np
 
-arr = np.random.randn(5, 5)
-print arr
-print arr[::2].sort(1)
-print arr[:, :-1] < arr[:, 1:]
+arr = pd.read_csv('Car_Data.txt')
+print i in arr['buying'] if i == 'vhigh'
